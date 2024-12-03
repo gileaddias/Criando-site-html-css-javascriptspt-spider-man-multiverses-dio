7b219781dcc4f64828cee64837ef86f99cd5eec0
gileaddias <gfarias.gpf@gmail.com>
2024-12-03 09:52:00 -0300

    criando efeitos com javascript e CSS

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,6 +1,21 @@
-function addEventListenersToCards() {
+function handleMouseEnter() {
+    this.classList.add('s-card--hovered');
+    document.body.id = `${this.id}-hovered`;
+  }
+  
+  function handleMouseLeave() {
+    this.classList.remove('s-card--hovered');
+    document.body.id = '';
+  }
+  
+  function addEventListenersToCards() {
     const cardElements = document.getElementsByClassName('s-card');
-    console.log(cardElements);
-}
-
-document.addEventListener("DOMContentLoaded",addEventListenersToCards, false);
+    
+    for (let index = 0; index < cardElements.length; index++) {
+      const card = cardElements[index];
+      card.addEventListener('mouseenter', handleMouseEnter);
+      card.addEventListener('mouseleave', handleMouseLeave);
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
